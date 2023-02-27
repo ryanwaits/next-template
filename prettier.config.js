c72f00f5d9cc24f3c922b5c6352828f1c5266aa6
Ryan Waits <ryan.waits@gmail.com>
2023-02-27 12:54:23 -0600

    fix issue with tailwind plugin and sort imports not working together

--- prettier.config.js
+++ prettier.config.js
@@ -1,3 +1,19 @@
+const pluginSortImports = require('@ianvs/prettier-plugin-sort-imports');
+const pluginTailwindCss = require('prettier-plugin-tailwindcss');
+
+/** @type {import("prettier").Parser}  */
+const parser = {
+  ...pluginSortImports.parsers.typescript,
+  parse: pluginTailwindCss.parsers.typescript.parse,
+};
+
+/** @type {import("prettier").Plugin}  */
+const myPlugin = {
+  parsers: {
+    typescript: parser,
+  },
+};
+
 /** @type {import('prettier').Config} */
 module.exports = {
   endOfLine: 'lf',
@@ -18,6 +34,7 @@ module.exports = {
     '^@/lib/(.*)$',
     '^@/components/(.*)$',
     '^@/ui/(.*)$',
+    '^@/hooks/(.*)$',
     '^@/styles/(.*)$',
     '^[./]',
   ],
@@ -27,8 +44,6 @@ module.exports = {
   importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
   importOrderMergeDuplicateImports: true,
   importOrderCombineTypeAndValueImports: true,
-  plugins: [
-    require('prettier-plugin-tailwindcss'),
-    require('@ianvs/prettier-plugin-sort-imports'),
-  ],
+  plugins: [myPlugin],
+  tailwindConfig: './tailwind.config.js',
 };
